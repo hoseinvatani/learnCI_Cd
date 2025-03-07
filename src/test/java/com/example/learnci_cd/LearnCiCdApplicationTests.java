817b357f23ce52f4c395129a1f6fbf660fb6645b
“HoseinVatani” <Hosein.vatani20@gmail.com>
2025-03-07 05:06:28 +0330

    write test for code

--- src/test/java/com/example/learnci_cd/LearnCiCdApplicationTests.java
+++ src/test/java/com/example/learnci_cd/LearnCiCdApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.learnci_cd;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LearnCiCdApplicationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testHelloEndpoint() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/api/hello", String.class);
+        assertThat(response.getBody()).isEqualTo("Hello, GitLab CI/CD!");
     }
 
 }
