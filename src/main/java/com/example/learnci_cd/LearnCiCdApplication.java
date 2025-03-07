6215fb7e069cc3ec920ea2015c9b75a71434f9ac
“HoseinVatani” <Hosein.vatani20@gmail.com>
2025-03-07 04:03:50 +0330

    push project

---
+++ src/main/java/com/example/learnci_cd/LearnCiCdApplication.java
@@ -0,0 +1,23 @@
+package com.example.learnci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class LearnCiCdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LearnCiCdApplication.class, args);
+    }
+}
+
+@RestController
+@RequestMapping("/api")
+class ApiController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, GitLab CI/CD!";
+    }
+}
---
+++ src/test/java/com/example/learnci_cd/LearnCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
